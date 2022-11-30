edb966bd8e8390871c5615be9e2caae534b70e99
Anmol Anand <anmolanand2k15@gmail.com>
2022-11-30 18:45:03 +0530

    Added Conditional Statement

---
+++ Conditional-Statement/if_else.dart
@@ -0,0 +1,11 @@
+void main() {
+  var a = 2;
+  var b = 3;
+  if (a > b) {
+    print("a is greater than b");
+  } else {
+    print("a is less than b");
+  }
+
+  // Nothing New In This Code Snippet
+}
---
+++ Conditional-Statement/switch_case.dart
@@ -0,0 +1,21 @@
+void main() {
+  var grade = "A";
+  switch (grade) {
+    case "A":
+      print("Excellent");
+      break;
+    case "B":
+      print("Good");
+      break;
+    case "C":
+      print("Fair");
+      break;
+    case "D":
+      print("Poor");
+      break;
+    default:
+      print("Invalid Grade");
+  }
+
+  // Nothing New In This Code Snippet
+}
